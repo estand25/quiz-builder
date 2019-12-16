835ca59e54d194bcbce2b87cd1198e269ead02cb
Standley Eugene <estand25@gmail.com>
2019-12-15 20:05:31 -0500

    Create production build early

---
+++ client/build/precache-manifest.6374a36ad524d41da2cb31ace1520e73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0f12b6cd2159ec1edb0ce6b64414cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6acda976c9affcef4e19",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6acda976c9affcef4e19",
+    "url": "/static/js/2.39de4fb8.chunk.js"
+  },
+  {
+    "revision": "192854085afe291ab396af681bd9f304",
+    "url": "/static/js/2.39de4fb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d95587757767eae0e85",
+    "url": "/static/js/main.61d66c30.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
